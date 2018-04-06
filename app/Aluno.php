657ead4613517ab2e60e6151f13c29e135859f10
Rafael Bordin <rafael.bordin@live.com>
2018-04-06 11:43:28 -0300

    Typo

--- app/Aluno.php
+++ app/Aluno.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Aluno extends Model
 {
-    protected $fillable = array('id', 'nome', 'serie','cep','rua', 'bairro', 'complemento', 'numero_endereco', 'cidade', 'estado','nome_mae', 'cpf', 'vec');
+    protected $fillable = array('id', 'nome', 'serie','cep','rua', 'bairro', 'complemento', 'numero_endereco', 'cidade', 'estado','nome_mae', 'cpf', 'venc');
 }
